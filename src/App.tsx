f2d5601e48b44c651b4931e54cb7d73e37f31288
Dartanpionze <Dartanpion@gmail.com>
2026-07-26 01:00:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './routes/index'
+import Club from './routes/club'
+import Equipes from './routes/equipes'
+import Calendrier from './routes/calendrier'
+import Actualites from './routes/actualites'
+import Galerie from './routes/galerie'
+import Partenaires from './routes/partenaires'
+import Contact from './routes/contact'
+
 function App() {
   return (
-    <h1>
-      FC Plouha fonctionne !
-    </h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/club" element={<Club />} />
+      <Route path="/equipes" element={<Equipes />} />
+      <Route path="/calendrier" element={<Calendrier />} />
+      <Route path="/actualites" element={<Actualites />} />
+      <Route path="/galerie" element={<Galerie />} />
+      <Route path="/partenaires" element={<Partenaires />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   )
 }
 
