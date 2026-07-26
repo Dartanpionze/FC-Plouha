feafbff3faaf5253c2f7103f7196901aeb90edc6
Dartanpionze <Dartanpion@gmail.com>
2026-07-26 18:38:00 +0200

    Update ClubCrest.tsx

--- src/components/ClubCrest.tsx
+++ src/components/ClubCrest.tsx
@@ -3,7 +3,7 @@ export function ClubCrest({ className = 'w-12 h-12' }: { className?: string }) {
     <img
       src="/logo.png"
       alt="Blason du Football Club Plouha"
-      className={`${className} object-contain`}
+      className={`${className} object-contain shrink-0`}
     />
   )
 }
