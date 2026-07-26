747fc6f36368d1e6429fb639690bb0acd30627b6
Dartanpionze <Dartanpion@gmail.com>
2026-07-26 03:33:49 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@tanstack/react-router'
+import { Link } from 'react-router-dom'
 import { Facebook, Instagram, Mail, MapPin, Phone } from 'lucide-react'
 import { ClubCrest } from './ClubCrest'
 import { club } from '@/data/club'
