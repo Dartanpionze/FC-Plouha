f2a9808ba1af9eb8bbe92fb900bde7cf3ea65ec8
Dartanpionze <Dartanpion@gmail.com>
2026-08-04 00:06:23 +0200

    Update Gallery.tsx

--- src/admin/pages/Gallery.tsx
+++ src/admin/pages/Gallery.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="p-8 text-white">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <h1 className="text-3xl font-bold">Gallery</h1>
     </div>
   )
 }
