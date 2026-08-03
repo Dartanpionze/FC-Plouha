9fa49a882b70c07639239f89978f7b7ab32011a3
Dartanpionze <Dartanpion@gmail.com>
2026-08-04 00:08:19 +0200

    Update Matches.tsx

--- src/admin/pages/Matches.tsx
+++ src/admin/pages/Matches.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="p-8 text-white">
-      <h1 className="text-3xl font-bold">Matche</h1>
+      <h1 className="text-3xl font-bold">Matches</h1>
     </div>
   )
 }
