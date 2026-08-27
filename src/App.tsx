4ba3025c76d3181e1f68d835d167fafc79d4ca3f
Dartanpionze <Dartanpion@gmail.com>
2026-08-27 23:23:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,18 @@ import GalleryPage from './routes/galerie'
 import SponsorsPage from './routes/partenaires'
 import Contact from './routes/contact'
 
-import AdminLayout from "./admin/layouts/AdminLayout";
-
-import Dashboard from "./admin/pages/Dashboard";
-import Login from "./admin/pages/Login";
-import News from "./admin/pages/News";
-import Teams from "./admin/pages/Teams";
-import Players from "./admin/pages/Players";
-import Matches from "./admin/pages/Matches";
-import Gallery from "./admin/pages/Gallery";
-import Partners from "./admin/pages/Partners";
-import Settings from "./admin/pages/Settings";
-import AdminClub from "./admin/pages/Club";
-import Registrations from "./admin/pages/Registrations";
+import AdminLayout from './admin/layouts/AdminLayout'
+import Dashboard from './admin/pages/Dashboard'
+import Login from './admin/pages/Login'
+import News from './admin/pages/News'
+import Teams from './admin/pages/Teams'
+import Players from './admin/pages/Players'
+import Matches from './admin/pages/Matches'
+import Gallery from './admin/pages/Gallery'
+import Partners from './admin/pages/Partners'
+import Settings from './admin/pages/Settings'
+import AdminClub from './admin/pages/Club'
+import Registrations from './admin/pages/Registrations'
 
 function App() {
   return (
@@ -33,6 +32,7 @@ function App() {
       <ScrollToTop />
 
       <Routes>
+        {/* SITE PUBLIC */}
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/club" element={<Club />} />
@@ -41,22 +41,25 @@ function App() {
           <Route path="/calendrier" element={<CalendarPage />} />
           <Route path="/actualites" element={<NewsPage />} />
           <Route path="/actualites/:id" element={<ArticlePage />} />
-          <Route path="/admin/login" element={<Login />} />          
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="news" element={<News />} />
-            <Route path="club" element={<AdminClub />} />
-            <Route path="teams" element={<Teams />} />
-            <Route path="players" element={<Players />} />
-            <Route path="registrations" element={<Registrations />} />
-            <Route path="matches" element={<Matches />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="partners" element={<Partners />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
           <Route path="/galerie" element={<GalleryPage />} />
           <Route path="/partenaires" element={<SponsorsPage />} />
           <Route path="/contact" element={<Contact />} />
+        </Route>
+
+        {/* ADMINISTRATION - séparée du Navbar/Footer public */}
+        <Route path="/admin/login" element={<Login />} />
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="news" element={<News />} />
+          <Route path="club" element={<AdminClub />} />
+          <Route path="teams" element={<Teams />} />
+          <Route path="players" element={<Players />} />
+          <Route path="registrations" element={<Registrations />} />
+          <Route path="matches" element={<Matches />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="partners" element={<Partners />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </>
