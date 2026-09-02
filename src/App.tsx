d192129960298b0e691a765d9fa0c67e9130346e
Dartanpionze <Dartanpion@gmail.com>
2026-09-03 00:38:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ import Matches from './admin/pages/Matches'
 import Gallery from './admin/pages/Gallery'
 import Partners from './admin/pages/Partners'
 import Pricing from './admin/pages/Pricing'
+import Trainings from './admin/pages/Trainings'
 import Settings from './admin/pages/Settings'
 import AdminClub from './admin/pages/Club'
 import Registrations from './admin/pages/Registrations'
@@ -74,6 +75,7 @@ function App() {
           <Route path="players" element={<RequireAdminPermission module="players"><Players /></RequireAdminPermission>} />
           <Route path="registrations" element={<RequireAdminPermission module="registrations"><Registrations /></RequireAdminPermission>} />
           <Route path="matches" element={<RequireAdminPermission module="matches"><Matches /></RequireAdminPermission>} />
+          <Route path="trainings" element={<RequireAdminPermission module="teams"><Trainings /></RequireAdminPermission>} />
           <Route path="gallery" element={<RequireAdminPermission module="gallery"><Gallery /></RequireAdminPermission>} />
           <Route path="partners" element={<RequireAdminPermission module="partners"><Partners /></RequireAdminPermission>} />
           <Route path="pricing" element={<RequireAdminPermission module="settings"><Pricing /></RequireAdminPermission>} />
