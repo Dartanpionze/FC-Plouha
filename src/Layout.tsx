c0ef22542b8159756b3b555b73863973e87989b3
Dartanpionze <Dartanpion@gmail.com>
2026-09-18 00:58:26 +0200

    Update Layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,9 +7,13 @@ import './styles.css'
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <a href="#contenu-principal" className="skip-link">
+        Aller au contenu principal
+      </a>
+
       <Navbar />
 
-      <main className="flex-1">
+      <main id="contenu-principal" tabIndex={-1} className="flex-1">
         <Outlet />
       </main>
 
